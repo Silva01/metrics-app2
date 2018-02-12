38374a8f6a9fec2cbd77e9f0aa9b62470c0e22f5
Silva01 <silva20102@gmail.com>
2018-02-11 22:46:27 -0200

    Construido tela de login

---
+++ src/components/login/LoginComponent.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <!-- Tabs Titles -->
+            <h2 class="active"> Sign In </h2>
+
+            <!-- Icon -->
+            <div class="fadeIn first">
+            <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+            </div>
+
+            <!-- Login Form -->
+            <form>
+            <input 
+                type="text" 
+                id="login" 
+                class="fadeIn second" 
+                name="login" 
+                placeholder="Usuário"
+                v-model="form.user">
+
+            <input 
+                type="password" 
+                id="password" 
+                class="fadeIn third" 
+                name="login" 
+                placeholder="Senha"
+                v-model="form.pass">
+
+            <input type="button" class="fadeIn fourth" value="Log In" v-on:click="get(form)">
+            </form>
+
+            <!-- Remind Passowrd -->
+            <div id="formFooter">
+            <a class="underlineHover" href="#">Forgot Password?</a>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+import Router from 'vue-router'
+
+export default {
+    name:'login-component',
+    data() {
+        return {
+            form: {
+                use: '',
+                pass: ''
+            }
+        }
+    },
+    methods:{
+        get: (form) => {
+
+            let router = new Router()
+            
+            if (form.user != "" && form.pass != "") {
+                sessionStorage.setItem('token', '123456');
+                router.push('Home')
+            } else {
+                router.push('/')
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    @import './loginComponent.css';
+</style>
