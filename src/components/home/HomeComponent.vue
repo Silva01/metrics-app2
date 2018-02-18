eebe28c8c43ee9d26744e1bfe26a8810ca8a8b31
Daniel Silva <silva20102@gmail.com>
2018-02-17 22:29:03 -0200

    Retirado componentes do bootstrap

--- src/components/home/HomeComponent.vue
+++ src/components/home/HomeComponent.vue
@@ -1,31 +1,13 @@
 <template>
     <div>        
-        <b-card title="Card Title"
-                img-src=""
-                img-alt="Image"
-                img-top
-                tag="article"
-                style="max-width: 20rem;"
-                class="mb-2">
-            <p class="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card>   
+        <h1>Teste</h1>
     </div>  
 </template>
 
 <script>
 
-import bCard from 'bootstrap-vue/es/components/card/card'
-import bButton from 'bootstrap-vue/es/components/button/button'
-
 export default {
-    name: 'home-component',
-    components: {
-        bCard,
-        bButton
-    }
+    name: 'home-component'
 }
 </script>
 
