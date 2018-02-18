6299a6a6094640be36ea3d6c496d1e769929bc36
Daniel Silva <silva20102@gmail.com>
2018-02-17 23:27:16 -0200

    Adicionado sass pre-processador e adicionado component card

--- src/components/home/HomeComponent.vue
+++ src/components/home/HomeComponent.vue
@@ -1,16 +1,49 @@
 <template>
-    <div>        
-        <h1>Teste</h1>
-    </div>  
+    <div class="grid-container">
+        <div class="grid-item">
+            <card :valor="quantQuestoes" :label="label1" :tipo="tipoPorcent"></card>
+        </div>
+        <div class="grid-item">
+            <card :valor="porcentAcertos" :label="label2" :tipo="tipoPorcent"></card>
+        </div>
+        <div class="grid-item">
+            <card :valor="pontosMes" :label="label3" :tipo="tipoPontos"></card>
+        </div>        
+    </div>   
 </template>
 
 <script>
 
+import Card from '../util/card/Card'
+
 export default {
-    name: 'home-component'
+    name: 'home-component',
+    components: {
+        Card
+    },
+    data(){
+        return {
+            quantQuestoes: 10,
+            porcentAcertos: 80,
+            pontosMes: 200,
+            label1: "Quantidade de Acertos",
+            label2: "Porcentagem de Acertos",
+            label3: "Pontos no Mês",
+            tipoPorcent: "%",
+            tipoPontos: "Pontos",
+        }
+    }
 }
 </script>
 
-<style >    
-
+<style>    
+    .grid-container {
+        display: grid;
+        grid-template-columns: auto auto auto;
+        margin: 0px 2% 3% 12%;
+    }
+    .grid-item {
+        font-size: 30px;
+        text-align: center;
+    }
 </style>
