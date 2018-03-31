37780655aaa281a62224c30d8b73634939e80077
Daniel Silva <silva20102@gmail.com>
2018-03-31 10:17:05 -0300

    Adicionado card para grafico

---
+++ src/components/util/card/CardGraphic.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span>{{ title }}</span>
+    </div>
+    <div>
+
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'card-graphic',
+    props: ['type', 'indicadores', 'title']
+  }
+</script>
+
+<style></style>
