a18fe0d3bb93d1bffeff3c02d9a1cbea7c209bc6
Daniel Silva <silva20102@gmail.com>
2018-04-14 18:53:26 -0300

    Add class metricas

---
+++ src/assets/classes/metricas.ts
@@ -0,0 +1,17 @@
+import Http from "./http";
+/**
+ * Created by DanielSilva on 14/04/18.
+ */
+
+export default class Metricas {
+
+  private http: Http;
+
+  constructor() {
+    this.http = new Http();
+  }
+
+  countingSuccessQuestions(params: Object): Promise<any> {
+    return this.http.post('http://metrics.graphic.success', JSON.stringify(params));
+  }
+}
